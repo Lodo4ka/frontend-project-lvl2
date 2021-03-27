a7bf72d3485f27e9c6e40d49bc1a567582e5227d
maxim.novikov <maxim.novikov@onlinesup.com>
2021-03-27 17:34:19 +0300

    add format flag

--- src/info.mjs
+++ src/info.mjs
@@ -3,7 +3,9 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-  .version('0.0.1', '-v, --vers', 'output the current version');
+  .version('0.0.1', '-v, --vers', 'output the current version')
+  .arguments('[type]')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
 
