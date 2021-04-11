ce63563a03830271eb3a645f14ff4c79b59ed764
lodo4ka <doomymax@gmail.com>
2021-04-11 21:27:45 +0300

    generate ast diff

--- src/diff.mjs
+++ src/diff.mjs
@@ -1,5 +1,5 @@
 import {
-  union, isEqual, has, isObject, get,
+  union, isEqual, has, isObject, get, isEmpty, uniq,
 } from 'lodash-es';
 
 export default (source1, source2) => {
@@ -9,25 +9,50 @@ export default (source1, source2) => {
   const keyValues1 = Object.entries(source1);
   const keyValues2 = Object.entries(source2);
   const unionKeyValues = union(keyValues1, keyValues2);
-  const generateDiffInfo = (values, parent) => {
-    const result = values.reduce((acc, [key, value]) => {
-      const currentKey = [...parent, key].join('.');
-      if (isObject(value)) {
-        const children = Object.entries(value);
-        acc[key] = { ...acc[key], ...generateDiffInfo(children, [...parent, key]) };
-      } else if (!has(source1, currentKey)) {
-        acc[key] = 'added';
-      } else if (!has(source2, currentKey)) {
-        acc[key] = 'deleted';
-      } else if (get(source1, currentKey) !== get(source2, currentKey)) {
-        acc[key] = 'changed';
-      } else {
-        acc[key] = 'unchanged';
-      }
-      return acc;
-    }, {});
+  const childrenSetStatus = (children, statusArg) => Object.entries(children)
+    .reduce((acc, [key, value]) => (!isEmpty(value)
+      ? childrenSetStatus(value, statusArg) : { ...acc, [key]: { ...value, status: statusArg } }));
+  const getStatuses = (children) => Object.values(children)
+    .map(({ status, childrenNode }) => (!isEmpty(childrenNode)
+      ? getStatuses(childrenNode) : status));
+  const hasChangeNodes = (childrenArg) => getStatuses(childrenArg).includes('changed');
+  const generateAstDiff = (values, parent) => {
+    const result = values
+      .reduce((acc, [key, value]) => {
+        const currentKey = [...parent, key].join('.');
+        let status = 'unchanged';
+        let children = {};
+        if (isObject(value)) {
+          const childrenNode = Object.entries(value);
+          children = generateAstDiff(childrenNode, [...parent, key]);
+          const areChangeNodes = hasChangeNodes(children);
+          const prevChildren = acc[key]?.children ?? {};
+          if (areChangeNodes) {
+            return { ...acc, [key]: { status, children: { ...prevChildren, ...children } } };
+          }
+          const statuses = uniq(getStatuses(children));
+          const hasSameStatus = statuses.length === 1;
+          if (hasSameStatus) {
+            return {
+              ...acc,
+              [key]: {
+                status: statuses[0],
+                children: { ...prevChildren, ...childrenSetStatus(children, status) },
+              },
+            };
+          }
+        }
+        if (!has(source1, currentKey)) {
+          status = 'added';
+        } else if (!has(source2, currentKey)) {
+          status = 'deleted';
+        } else if (get(source1, currentKey) !== get(source2, currentKey)) {
+          status = 'changed';
+        }
+        return { ...acc, [key]: { status, children } };
+      }, {});
     return result;
   };
-  const diffInfo = generateDiffInfo(unionKeyValues, []);
+  const diffInfo = generateAstDiff(unionKeyValues, []);
   return { diffInfo, source1, source2 };
 };
--- src/formatter/stylish.mjs
+++ src/formatter/stylish.mjs
@@ -1,36 +1,61 @@
 import {
-  sortBy, isObject, get,
+  sortBy, isEmpty, get,
 } from 'lodash-es';
 
 export default (diffInfo, source1, source2) => {
   const replacer = ' ';
   const spaceCount = 2;
+  const hasChildren = (children) => !isEmpty(children);
+
   const sortCustom = (obj) => sortBy(Object.entries(obj), ([key]) => key)
-    .map(([key, val]) => (isObject(val) ? [key, sortCustom(val)] : [key, val]));
+    .map(([key, val]) => (hasChildren(val.children)
+      ? [key, { ...val, children: sortCustom(val.children) }] : [key, val]));
   const sortDiffInfo = sortCustom(diffInfo);
-  const createStylishLines = (diff, depth, parent) => diff.reduce((acc, [key, value]) => {
-    const currentKey = [...parent, key].join('.');
-    const indentSize = depth * spaceCount;
-    const elemeIndent = replacer.repeat((indentSize));
-    const bracketIndent = replacer.repeat(indentSize - spaceCount);
-    if (Array.isArray(value)) {
-      return [...acc, `${elemeIndent}${key}: {`,
-        ...createStylishLines(value, depth + 1,
-          [...parent, key]), `${bracketIndent}}`];
-    }
-    if (value === 'added') {
-      return [...acc, `${elemeIndent}+ ${key}: ${get(source2, currentKey)}`];
-    }
-    if (value === 'deleted') {
-      return [...acc, `${elemeIndent}- ${key}: ${get(source1, currentKey)}`];
-    }
-    if (value === 'changed') {
-      return [...acc, `${elemeIndent}- ${key}: ${get(source1, currentKey)}`, `${elemeIndent}+ ${key}: ${get(source2, currentKey)}`];
-    }
-    return [...acc, `${elemeIndent}  ${key}: ${get(source1, currentKey)}`];
-  }, []);
+  const createStylishLines = (diff, depth, parent) => diff
+    .reduce((acc, [key, { status, children }]) => {
+      const currentKey = [...parent, key].join('.');
+      const indentSize = depth * spaceCount;
+      const elemeIndent = replacer.repeat((indentSize));
+      const bracketIndent = replacer.repeat(indentSize - spaceCount);
+
+      const drawLeaf = (symbol, keyNode, pathKey, node, indent) => [`${indent.element}${symbol} ${keyNode}: ${get(node, pathKey)}`];
+      const drawChildren = (childrenNode, symbol, keyNode, pathKey, depthNode, indent) => [
+        `${indent.element}${symbol} ${keyNode}: {`,
+        ...createStylishLines(childrenNode, depthNode + 1,
+          pathKey.split('.')), `${indent.bracket}}`,
+      ];
+      const drawNode = ({
+        childrenNode, symbol, keyNode, pathKey, node, indent, depthNode,
+      }) => (
+        hasChildren(childrenNode)
+          ? drawChildren(childrenNode, symbol, keyNode, pathKey, depthNode, indent)
+          : drawLeaf(symbol, keyNode, pathKey, node, indent)
+      );
+      if (status === 'added') {
+        return [...acc,
+          ...drawNode({
+            childrenNode: children, symbol: '+', keyNode: key, pathKey: currentKey, node: source2, indent: { element: elemeIndent, bracket: bracketIndent }, depthNode: depth,
+          })];
+      } if (status === 'deleted') {
+        return [...acc,
+          ...drawNode({
+            childrenNode: children, symbol: '-', keyNode: key, pathKey: currentKey, node: source1, indent: { element: elemeIndent, bracket: bracketIndent }, depthNode: depth,
+          })];
+      } if (status === 'changed') {
+        return [...acc,
+          ...drawNode({
+            childrenNode: children, symbol: '+', keyNode: key, pathKey: currentKey, node: source2, indent: { element: elemeIndent, bracket: bracketIndent }, depthNode: depth,
+          }),
+          ...drawNode({
+            childrenNode: children, symbol: '-', keyNode: key, pathKey: currentKey, node: source1, indent: { element: elemeIndent, bracket: bracketIndent }, depthNode: depth,
+          })];
+      }
+      return [...acc,
+        ...drawNode({
+          childrenNode: children, symbol: ' ', keyNode: key, pathKey: currentKey, node: source1, indent: { element: elemeIndent, bracket: bracketIndent }, depthNode: depth,
+        })];
+    }, []);
   const lines = createStylishLines(sortDiffInfo, 2, []);
-  console.log('lines', lines);
   return [
     '{',
     ...lines,
