e273010c258188b9494c0482793a3b2556eac68a
lodo4ka <doomymax@gmail.com>
2021-06-27 21:44:59 +0300

    add different describe section

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -20,66 +20,70 @@ describe('check parser with all formatters', () => {
   const nestedAnswerCI = readFile('result_stylish_ci.txt');
   const plainNestedCI = readFile('result_plain_ci.txt');
 
-  test('compare two plain json', () => {
-    const file1 = getFixturePath('ex1.json');
-    const file2 = getFixturePath('ex2.json');
-    const result = diff(file1, file2, 'common');
-    expect(result).toEqual(answerPlain);
+  describe('check json file', () => {
+    test('compare two plain json', () => {
+      const file1 = getFixturePath('ex1.json');
+      const file2 = getFixturePath('ex2.json');
+      const result = diff(file1, file2, 'common');
+      expect(result).toEqual(answerPlain);
+    });
+
+    test('compare two nested json with format stylishFormatter', () => {
+      const file1 = getFixturePath('file1.json');
+      const file2 = getFixturePath('file2.json');
+      const result = diff(file1, file2, 'stylish');
+      expect(result).toEqual(asnwerNested);
+    });
+
+    test('compare two nested json with format stylish', () => {
+      const file1 = getFixturePath('file1_ci.json');
+      const file2 = getFixturePath('file2_ci.json');
+      const result = diff(file1, file2, 'stylish');
+      expect(result).toEqual(nestedAnswerCI);
+    });
+
+    test('compare two nested json with format plain', () => {
+      const file1 = getFixturePath('file1_ci.json');
+      const file2 = getFixturePath('file2_ci.json');
+      const result = diff(file1, file2, 'plain');
+      expect(result).toEqual(plainNestedCI);
+    });
   });
 
-  test('compare two plain yml', () => {
-    const file1 = getFixturePath('filepath1.yml');
-    const file2 = getFixturePath('filepath2.yml');
-    const result = diff(file1, file2, 'common');
-    expect(result).toEqual(answerPlain);
-  });
+  describe('check yml file', () => {
+    test('compare two plain yml', () => {
+      const file1 = getFixturePath('filepath1.yml');
+      const file2 = getFixturePath('filepath2.yml');
+      const result = diff(file1, file2, 'common');
+      expect(result).toEqual(answerPlain);
+    });
 
-  test('compare two nested json with format stylishFormatter', () => {
-    const file1 = getFixturePath('file1.json');
-    const file2 = getFixturePath('file2.json');
-    const result = diff(file1, file2, 'stylish');
-    expect(result).toEqual(asnwerNested);
-  });
+    test('compare two nested yml with format plain', () => {
+      const file1 = getFixturePath('file1y.yml');
+      const file2 = getFixturePath('file2y.yml');
+      const result = diff(file1, file2, 'plain');
+      expect(result).toEqual(answerPlainNested);
+    });
 
-  test('compare two nested yml with format plain', () => {
-    const file1 = getFixturePath('file1y.yml');
-    const file2 = getFixturePath('file2y.yml');
-    const result = diff(file1, file2, 'plain');
-    expect(result).toEqual(answerPlainNested);
-  });
+    test('compare two nested yml with format json', () => {
+      const file1 = getFixturePath('file1y.yml');
+      const file2 = getFixturePath('file2y.yml');
+      const result = diff(file1, file2, 'json');
+      expect(result).toEqual(nestedJsonAnswer);
+    });
 
-  test('compare two nested yml with format json', () => {
-    const file1 = getFixturePath('file1y.yml');
-    const file2 = getFixturePath('file2y.yml');
-    const result = diff(file1, file2, 'json');
-    expect(result).toEqual(nestedJsonAnswer);
-  });
+    test('compare two nested yml with format stylish', () => {
+      const file1 = getFixturePath('file1_ci.yml');
+      const file2 = getFixturePath('file2_ci.yml');
+      const result = diff(file1, file2, 'stylish');
+      expect(result).toEqual(nestedAnswerCI);
+    });
 
-  test('compare two nested json with format stylish', () => {
-    const file1 = getFixturePath('file1_ci.json');
-    const file2 = getFixturePath('file2_ci.json');
-    const result = diff(file1, file2, 'stylish');
-    expect(result).toEqual(nestedAnswerCI);
-  });
-
-  test('compare two nested yml with format stylish', () => {
-    const file1 = getFixturePath('file1_ci.yml');
-    const file2 = getFixturePath('file2_ci.yml');
-    const result = diff(file1, file2, 'stylish');
-    expect(result).toEqual(nestedAnswerCI);
-  });
-
-  test('compare two nested yml with format plain', () => {
-    const file1 = getFixturePath('file1_ci.yml');
-    const file2 = getFixturePath('file2_ci.yml');
-    const result = diff(file1, file2, 'plain');
-    expect(result).toEqual(plainNestedCI);
-  });
-
-  test('compare two nested json with format plain', () => {
-    const file1 = getFixturePath('file1_ci.json');
-    const file2 = getFixturePath('file2_ci.json');
-    const result = diff(file1, file2, 'plain');
-    expect(result).toEqual(plainNestedCI);
+    test('compare two nested yml with format plain', () => {
+      const file1 = getFixturePath('file1_ci.yml');
+      const file2 = getFixturePath('file2_ci.yml');
+      const result = diff(file1, file2, 'plain');
+      expect(result).toEqual(plainNestedCI);
+    });
   });
 });
