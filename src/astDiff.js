c5e7e60104c52a4aa14d098725608352b362e40a
lodo4ka <doomymax@gmail.com>
2021-07-04 22:48:26 +0300

    refactor nati ve sort on sortBy from lodash

--- src/astDiff.js
+++ src/astDiff.js
@@ -1,10 +1,10 @@
 import {
-  union, has, isObject, get, keys,
+  union, has, isObject, get, keys, sortBy,
 } from 'lodash-es';
 
 const generateAstDiff = (obj1, obj2) => {
   const unionKeys = union(keys(obj1), keys(obj2));
-  return unionKeys.sort().map((key) => {
+  return sortBy(unionKeys).map((key) => {
     if (isObject(get(obj1, key)) && isObject(get(obj2, key))) {
       return {
         key,
