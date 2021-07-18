d0d8fda7dfbf79ac0d644c32fd5df147d02e0a6b
lodo4ka <doomymax@gmail.com>
2021-07-18 21:58:11 +0300

    expand maps from functions

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -2,13 +2,12 @@ import stylishFormatter from './stylish.js';
 import plainFormatter from './plain.js';
 import jsonFormatter from './json.js';
 
-const getFormatter = (choisesFormatter) => {
-  const mapFormatter = {
-    stylish: stylishFormatter,
-    plain: plainFormatter,
-    json: jsonFormatter,
-  };
-  return mapFormatter[choisesFormatter];
+const mapFormatter = {
+  stylish: stylishFormatter,
+  plain: plainFormatter,
+  json: jsonFormatter,
 };
+
+const getFormatter = (choisesFormatter) => mapFormatter[choisesFormatter];
 
 export default getFormatter;
--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,12 +1,12 @@
 import jsonParser from './json-parser.js';
 import ymlParser from './yml-parser.js';
 
-const getParser = (typeFormatter) => {
-  const parsers = {
-    yml: ymlParser,
-    json: jsonParser,
-  };
-  return parsers[typeFormatter];
+const parsers = {
+  yml: ymlParser,
+  yaml: ymlParser,
+  json: jsonParser,
 };
+
+const getParser = (typeFormatter) => parsers[typeFormatter];
 
 export default getParser;
