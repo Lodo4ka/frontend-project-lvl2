81a17463917fe826231f005760f3c12a051ec962
lodo4ka <doomymax@gmail.com>
2021-07-18 21:28:24 +0300

    parse each file separately

--- src/diff.js
+++ src/diff.js
@@ -5,12 +5,17 @@ import getExtensionName from './getExtensionName.js';
 import getParser from './parsers/index.js';
 import getFormatter from './formatter/index.js';
 
-export default (path1, path2, choisesFormatter = 'stylish') => {
-  const data1 = fs.readFileSync(path1, { encoding: 'utf8', flag: 'r' });
-  const data2 = fs.readFileSync(path2, { encoding: 'utf8', flag: 'r' });
-  const extName = getExtensionName(path1);
+const getParsedData = (path) => {
+  const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+  const extName = getExtensionName(path);
   const parse = getParser(extName);
-  const [source1, source2] = parse(data1, data2);
+  const parsedData = parse(data);
+  return parsedData;
+};
+
+export default (path1, path2, choisesFormatter = 'stylish') => {
+  const source1 = getParsedData(path1);
+  const source2 = getParsedData(path2);
   const ast = astDiff(source1, source2);
   const formatter = getFormatter(choisesFormatter);
   return formatter(ast);
--- src/parsers/yml-parser.js
+++ src/parsers/yml-parser.js
@@ -1,3 +1,3 @@
 import yaml from 'js-yaml';
 
-export default (source1, source2) => [source1, source2].map((source) => yaml.load(source));
+export default (source) => yaml.load(source);
